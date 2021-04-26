5e0b2d02e9cfbf120997be61d26aff5c5baea525
Arjun <arj.sna@gmail.com>
2021-04-26 23:16:42 +0530

    add forum and category schema

---
+++ flaskbb/management/schemas.py
@@ -0,0 +1,24 @@
+from marshmallow import fields
+from flaskbb.extensions import ma
+
+
+class ForumSchema(ma.Schema):
+    id = fields.Integer()
+    title = fields.String()
+    description = fields.String()
+    position = fields.Integer()
+    locked = fields.Boolean()
+    show_moderators = fields.Boolean()
+    post_count = fields.Integer()
+    topic_count = fields.Integer()
+    last_post_title = fields.String()
+    last_post_username = fields.String()
+    last_post_created = fields.DateTime()
+
+
+class CategorySchema(ma.Schema):
+    id = fields.Integer()
+    title = fields.String()
+    description = fields.String()
+    position = fields.Integer()
+    forums = fields.List(fields.Nested(ForumSchema))
