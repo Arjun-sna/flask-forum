4dd7e5927e5cf165c45a241ca6adc98a49758d20
Arjun <arj.sna@gmail.com>
2021-05-07 10:24:03 +0530

    update forum schemas

--- flaskbb/management/schemas.py
+++ flaskbb/management/schemas.py
@@ -1,4 +1,5 @@
 from marshmallow import fields
+from marshmallow.validate import Length
 from flaskbb.extensions import ma, db
 
 from flaskbb.forum.models import Forum, Category
@@ -12,28 +13,15 @@ class GroupSchema(ma.SQLAlchemyAutoSchema):
         sqla_session = db.session
 
 
-class CategorySchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = Category
-        load_instance = True
-        sqla_session = db.session
-
-
-class ForumInputSchema(ma.SQLAlchemySchema):
-    class Meta:
-        model = Forum
-        load_instance = True
-        sqla_session = db.session
-
-    # category_id = fields.Integer()
-    category = fields.Nested("CategorySchema", only=('id',), required=True)
-    title = fields.String()
-    description = fields.String()
-    position = fields.Integer()
-    locked = fields.Boolean()
-    show_moderators = fields.Boolean()
+class ForumInputSchema(ma.Schema):
+    category = fields.Integer(required=True)
+    title = fields.String(required=True, validate=Length(3))
+    description = fields.String(required=True, validate=Length(5))
+    position = fields.Integer(default=1)
+    locked = fields.Boolean(default=False)
+    show_moderators = fields.Boolean(default=False)
     external = fields.String()
-    groups = fields.Nested("GroupSchema", many=True, only=('id',))
+    groups = fields.List(fields.Integer(required=True), required=True)
 
 
 class ForumSchema(ma.Schema):
