8e7434863a603acc585a92e265b6d3dbd867f3e3
Arjun <arj.sna@gmail.com>
2021-05-08 13:21:13 +0530

    clean code

--- flaskbb/management/views.py
+++ flaskbb/management/views.py
@@ -41,11 +41,10 @@ from flaskbb.utils.requirements import (CanBanUser, CanEditUser, IsAdmin,
                                         IsAtleastModerator,
                                         IsAtleastSuperModerator)
 from flaskbb.utils.settings import flaskbb_config
-from .schemas import CategorySchema, ForumInputSchema
+from .schemas import CategorySchema
 from .services.forum import ForumManager
 
 category_schema = CategorySchema(many=True)
-forum_input_schema = ForumInputSchema()
 impl = HookimplMarker('flaskbb')
 
 logger = logging.getLogger(__name__)
