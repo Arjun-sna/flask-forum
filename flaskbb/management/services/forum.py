8dc11c06137823dfb411d99ac4177398e56a4e89
Arjun <arj.sna@gmail.com>
2021-05-08 13:14:59 +0530

    add update forum method in forum manager

--- flaskbb/management/services/forum.py
+++ flaskbb/management/services/forum.py
@@ -1,4 +1,6 @@
-from ..schemas import ForumInputSchema
+from sqlalchemy.orm.session import make_transient, make_transient_to_detached
+
+from ..schemas import ForumInputSchema, ForumUpdateSchema
 from ...forum.models import Forum, Category
 from ...user.models import Group
 from ...core.exceptions import ValidationError
@@ -7,8 +9,9 @@ from ...core.exceptions import ValidationError
 class ForumManager():
     def __init__(self):
         self.forum_input_schema = ForumInputSchema()
+        self.forum_update_schema = ForumUpdateSchema()
 
-    def validate_category(self, category_id):
+    def __fetch_category(self, category_id):
         category = Category.query.get(category_id)
 
         if not category:
@@ -17,7 +20,7 @@ class ForumManager():
 
         return category
 
-    def fetch_groups(self, groups_ids):
+    def __fetch_groups(self, groups_ids):
         groups = Group.query.filter(Group.id.in_(groups_ids)).all()
         if len(groups_ids) != len(groups):
             raise ValidationError(
@@ -26,10 +29,18 @@ class ForumManager():
 
     def addForum(self, forum_data):
         data = self.forum_input_schema.load(forum_data)
-        data['category'] = self.validate_category(data['category'])
-        data['groups'] = self.fetch_groups(data['groups'])
+        data['category'] = self.__fetch_category(data['category'])
+        data['groups'] = self.__fetch_groups(data['groups'])
         return self.save(data)
 
-    def save(self, forum_data):
+    def updateForum(self, forum_data):
+        data = self.forum_update_schema.load(forum_data)
+        if 'category' in data:
+            data['category'] = self.__fetch_category(data['category'])
+        if 'groups' in data:
+            data['groups'] = self.__fetch_groups(data['groups'])
+        return self.save(data, transient=True)
+
+    def save(self, forum_data, transient=False):
         forum = Forum(**forum_data)
         return forum.save()
